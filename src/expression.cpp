9040aa532eff45023afe117c2342c0359336823e
Vedang Vivek Dicholkar <107719294+VLambda@users.noreply.github.com>
2024-04-30 09:30:33 -0400

    Update expression.cpp

--- src/expression.cpp
+++ src/expression.cpp
@@ -11,24 +11,80 @@ String analyzeBytes(uint32_t buttonBits) {
 
     // Interpret the button bits using a switch statement
     switch (buttonBits) {
-        case 0b0111111101111111: // Enter
-            // Implement your logic for Enter button
-            result = "Enter";
+        case 0b011111111111111111111111:
+            result = "Menu";
             break;
-        case 0b1011111110111111: // +
-            // Implement your logic for + button
-            result = "+";
+        case 0b101111111111111111111111:
+            result = "Left";
             break;
-        case 0b110111111111111111111111: // -
-            // Implement your logic for - button
+        case 0b110111111111111111111111:
+            result = "+"
+            break;
+        case 0b111011111111111111111111:
             result = "-";
             break;
-            // Add cases for other buttons as needed
-            // ...
-
+        case 0b111101111111111111111111:
+            result = "7";
+            break;
+        case 0b111110111111111111111111:
+            result = "4";
+            break;
+        case 0b111111011111111111111111:
+            result = "1";
+            break;
+        case 0b111111101111111111111111:
+            result = "0";
+            break;
+        case 0b111111110111111111111111:
+            result = "Up";
+            break;
+        case 0b111111111011111111111111:
+            result = "Down";
+            break;
+        case 0b111111111101111111111111:
+            result = "*";
+            break;
+        case 0b111111111110111111111111:
+            result = "/";
+            break;
+        case 0b111111111111011111111111:
+            result = "8";
+            break;
+        case 0b111111111111101111111111:
+            result = "5";
+            break;
+        case 0b111111111111110111111111:
+            result = "2";
+            break;
+        case 0b111111111111111011111111:
+            result = ".";
+            break;
+        case 0b111111111111111101111111:
+            result = "Shift";
+            break;
+        case 0b111111111111111110111111:
+            result = "Right";
+            break;
+        case 0b111111111111111111011111:
+            result = "Power";
+            break;
+        case 0b111111111111111111101111:
+            result = "x";
+            break;
+        case 0b111111111111111111110111:
+            result = "9";
+            break;
+        case 0b111111111111111111111011:
+            result = "6";
+            break;
+        case 0b111111111111111111111101:
+            result = "3";
+            break;
+        case 0b111111111111111111111110:
+            result = "Enter";
+            break;
         default:
             // Handle unrecognized button combinations
-            result = "Unknown";
             break;
     }
 
